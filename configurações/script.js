9c002c70974925615c610829b226f01aca808733
van-dall <higorvandall@gmail.com>
2023-04-25 19:28:30 -0300

    Menu Cliente

---
+++ configurações/script.js
@@ -0,0 +1,4 @@
+const dropdownElementList = document.querySelectorAll(".dropdown-toggle");
+const dropdownList = [...dropdownElementList].map(
+  (dropdownToggleEl) => new bootstrap.Dropdown(dropdownToggleEl)
+);

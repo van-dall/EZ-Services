3832349e2f5ba9fba01fb2e67832ad2af0db236e
Noreh0 <heronricardo70@gmail.com>
2023-05-31 20:28:49 -0300

    atualizacao_php_2

---
+++ configurações/delete_prestador.php
@@ -0,0 +1,33 @@
+<?php
+
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$database = "EZservice";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) {
+  die("Conexão falhou: " . mysqli_connect_error());
+}
+if (isset($_POST['id'])) {
+  $id = intval(filter_var($_POST['id'], FILTER_VALIDATE_INT));
+
+  if ($id <= 0) {
+    die("ID inválido!");
+  }
+
+  $conn = mysqli_connect("localhost:3306", "root", "", "EZservice");
+
+  $sql = "DELETE FROM Prestador WHERE id = $id";
+
+  if (mysqli_query($conn, $sql)) {
+    echo "Registro excluído com sucesso!";
+  } else {
+    echo "Erro ao excluir registro: " . mysqli_error($conn);
+  }
+
+  mysqli_close($conn);
+}
+
+?>

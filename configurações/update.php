d445035ef9b859ff54696743cc0cbb7f9e1f9883
Noreh0 <heronricardo70@gmail.com>
2023-05-31 21:06:11 -0300

    atualizacao_telas_e_php

--- configurações/update.php
+++ configurações/update.php
@@ -5,22 +5,10 @@ $username = "root";
 $password = "";
 $database = "EZservice";
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+session_start(); 
 
 if (!$conn) {
     die("Conexão falhou: " . mysqli_connect_error());
 }
-
-$id = $_POST['ID_cliente'];
-$celular = $_POST['Celular_Cliente'];
-$endereco = $_POST['Endereco_Cliente'];
-$cep = $_POST['CEP_Cliente'];
-
-$sql = "UPDATE Cliente SET Celular_Cliente='$cliente', Endereco_Cliente='$endereco', CEP_Cliente='$cep'  WHERE id=$id";
-
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_affected_rows($conn) > 0)
-    echo "As informações foram atualizadas!"
-
-?>
